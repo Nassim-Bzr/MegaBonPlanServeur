cb66f0128b1f8e13eb56280834e2c8d0cfb028b1
Nassim-Bzr <nassimbzr78@gmail.com>
2024-05-27 11:41:19 +0200

    Fix case sensitivity issue in file names

---
+++ models/codePromo.model.js
@@ -0,0 +1,45 @@
+// models/codePromo.model.js
+
+module.exports = (sequelize, DataTypes) => {
+  const CodePromo = sequelize.define("CodePromo", {
+      id_codepromo: {
+          type: DataTypes.INTEGER,
+          primaryKey: true,
+          autoIncrement: true
+      },
+      code: {
+          type: DataTypes.STRING
+      },
+      description: {
+          type: DataTypes.TEXT
+      },
+      dateexpiration: {
+          type: DataTypes.DATE
+      },
+      id_utilisateur: {
+          type: DataTypes.INTEGER
+      },
+      approuvéparadmin: {
+          type: DataTypes.BOOLEAN
+      },
+      marchand:{
+        type: DataTypes.TEXT
+      },
+      imgmarchand:{
+        type: DataTypes.STRING
+      },
+
+      reduction:{
+        type: DataTypes.STRING
+      },
+
+      montant:{
+        type: DataTypes.STRING
+      },
+  }, {
+      tableName: 'codepromo',
+      timestamps: false
+  });
+
+  return CodePromo;
+};
