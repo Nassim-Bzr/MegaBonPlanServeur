6120a166be732b62a1a89f9b97974f1c1905ca55
Nassim-Bzr <nassimbzr78@gmail.com>
2024-05-27 12:18:44 +0200

    update bdd deploy ok

--- models/index.js
+++ models/index.js
@@ -1,17 +1,27 @@
 const Sequelize = require('sequelize');
-const config = require('../config/db.config.js');
+const dotenv = require('dotenv');
 
+// Charger les variables d'environnement à partir du fichier .env en développement
+if (process.env.NODE_ENV !== 'production') {
+  dotenv.config();
+}
 
-const sequelize = new Sequelize(config.DB, config.USER, config.PASSWORD, {
-  host: config.HOST,
-  dialect: config.dialect,
-  operatorsAliases: false,
+const isProduction = process.env.NODE_ENV === 'production';
 
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: 'postgres',
+  protocol: 'postgres',
+  dialectOptions: {
+    ssl: isProduction ? {
+      require: true,
+      rejectUnauthorized: false
+    } : false
+  },
   pool: {
-    max: config.pool.max,
-    min: config.pool.min,
-    acquire: config.pool.acquire,
-    idle: config.pool.idle
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000
   }
 });
 
@@ -19,41 +29,36 @@ const db = {};
 
 db.Sequelize = Sequelize;
 db.sequelize = sequelize;
+
+// Test de connexion
 sequelize.authenticate()
   .then(() => console.log('Connexion à la base de données établie avec succès.'))
   .catch(err => console.error('Impossible de se connecter à la base de données:', err));
 
+// Modèles
 db.favoris = require("./favoris.model.js")(sequelize, Sequelize);
 db.utilisateurs = require("./utilisateur.model.js")(sequelize, Sequelize);
 db.categories = require("./categorie.model.js")(sequelize, Sequelize);
 db.bonplans = require("./bonplan.model.js")(sequelize, Sequelize);
 db.commentaires = require("./commentaire.model.js")(sequelize, Sequelize);
-
 db.codepromos = require("./CodePromo.model.js")(sequelize, Sequelize);
 db.discussions = require("./discussion.model.js")(sequelize, Sequelize);
 
 // Associations
 db.bonplans.belongsTo(db.utilisateurs, { foreignKey: 'id_utilisateur' });
 db.utilisateurs.hasMany(db.bonplans, { foreignKey: 'id_utilisateur' });
-
 db.bonplans.hasMany(db.favoris, { as: 'Favoris', foreignKey: 'id_bonplan' });
 db.favoris.belongsTo(db.bonplans, { as: 'BonPlan', foreignKey: 'id_bonplan' });
-
-// Associations entre BonPlans et Commentaires
 db.bonplans.hasMany(db.commentaires, {
-  foreignKey: 'id_bonplan', // Assurez-vous que le nom de la clé étrangère est correct
-  as: 'commentaires' // Cela permettra d'accéder aux commentaires par bonplan.commentaires
+  foreignKey: 'id_bonplan',
+  as: 'commentaires'
 });
-
 db.commentaires.belongsTo(db.bonplans, {
   foreignKey: 'id_bonplan',
-  as: 'bonplan' // Cela permettra d'accéder au bon plan associé à un commentaire par commentaire.bonplan
+  as: 'bonplan'
 });
 db.bonplans.belongsTo(db.categories, { foreignKey: 'id_categorie' });
 db.categories.hasMany(db.bonplans, { foreignKey: 'id_categorie' });
-
-
-
 db.codepromos.belongsTo(db.utilisateurs, { foreignKey: 'id_utilisateur', as: 'Utilisateur' });
 db.utilisateurs.hasMany(db.codepromos, { foreignKey: 'id_utilisateur', as: 'CodePromos' });
 
