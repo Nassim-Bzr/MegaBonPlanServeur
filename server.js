53cb650dbd5874f3643a0de0c387608bf66eb9f7
Nassim-Bzr <nassimbzr78@gmail.com>
2024-05-27 12:27:12 +0200

    Add database synchronization

--- server.js
+++ server.js
@@ -7,9 +7,12 @@ const app = express();
 require('dotenv').config();
 const multer = require('multer');
 const path = require('path');
+const db = require('./models'); // Importez les modèles
 const favorisRoutes = require('./routes/favoris.routes');
 
-
+var corsOptions = {
+  origin: "*", // Autoriser toutes les origines
+};
 
 app.use(session({
   secret: 'votre secret',
@@ -23,13 +26,6 @@ app.use(passport.session());
 app.use(express.json());
 app.use(cors(corsOptions));
 
-// Remplacez ces lignes si vous utilisez Sequelize et non directement pool
-// const pool = require("./config/db.config.js");
-var corsOptions = {
-  origin: "*", // Autoriser toutes les origines
-};
-
-// Activation du middleware CORS avec les options configurées
 // Importez vos routeurs
 require("./routes/category.routes")(app);
 require("./routes/bonplan.routes")(app);
@@ -39,47 +35,16 @@ require("./routes/discussions.routes")(app);
 require("./routes/utilisateur.routes")(app);
 require("./routes/favoris.routes")(app);
 
-
-
-// Répétez pour d'autres entités en important leurs routeurs correspondants
-
 function isLoggedIn(req, res, next) {
   req.user ? next() : res.sendStatus(401);
 }
-
-/* app.get("/", (req, res) => {
-  res.json({ message: "Bonsoir." });
-});
- */
-
-/* // Configuration de Multer pour stocker les fichiers téléchargés
-const storage = multer.diskStorage({
-  destination: function(req, file, cb) {
-      cb(null, 'uploads/')  // Assurez-vous que ce dossier existe et est accessible en écriture
-  },
-  filename: function(req, file, cb) {
-      const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
-      cb(null, file.fieldname + '-' + uniqueSuffix + '.' + file.originalname.split('.').pop());
-  }
-});
-
-
-const upload = multer({ storage: storage }); */
-
-/* app.post('/upload', upload.single('imglink'), (req, res) => {
-  res.send('Fichier uploadé avec succès!');
-}); */
 
 app.get("/", (req, res) => {
   res.send('<a href="/auth/google"> Auth with google auth</a>');
 });
 
 app.get('/protected', isLoggedIn, (req, res) => {
-  // Supposons que "http://localhost:3000" soit l'URL de base de votre front-end
-  // Vous pouvez également utiliser process.env.CLIENT_URL pour cela
   const baseUrl = process.env.CLIENT_URL || "http://localhost:3000";
-  
-  // Redirigez vers le front-end avec des informations sur l'utilisateur
   res.redirect(`${baseUrl}/login-success?name=${req.user.displayName}`);
 });
 
@@ -88,16 +53,20 @@ app.get(
   passport.authenticate("google", { scope: ["email", "profile"] })
 );
 
-app.get( '/google/callback',
-  passport.authenticate( 'google', {
+app.get('/google/callback',
+  passport.authenticate('google', {
     successRedirect: '/protected',
     failureRedirect: '/auth/google/failure'
   })
 );
 
-
-
 const PORT = process.env.PORT || 8080;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}.`);
+
+// Synchronisez la base de données et démarrez le serveur
+db.sequelize.sync().then(() => {
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}.`);
+  });
+}).catch(err => {
+  console.error('Unable to connect to the database:', err);
 });
