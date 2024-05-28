8a450192a505e49b30a9282c3aea76a7876ab5fa
Nassim-Bzr <nassimbzr78@gmail.com>
2024-05-28 13:53:48 +0200

    update

--- controllers/utilisateur.controller.js
+++ controllers/utilisateur.controller.js
@@ -122,6 +122,7 @@ exports.update = async (req, res) => {
         if (updatedRows === 1) {
             res.send({ message: "Utilisateur mis à jour avec succès." });
         } else {
+            
             res.status(404).send({ message: "Utilisateur non trouvé." });
         }
     } catch (err) {
@@ -178,20 +179,20 @@ exports.login = async (req, res) => {
 
     try {
         const utilisateur = await Utilisateur.findOne({ where: { email } });
-
+    
         if (!utilisateur) {
             return res.status(404).send({ message: "Utilisateur non trouvé." });
         }
-
+    
         if (!utilisateur.isverified) {
             return res.status(401).send({ message: "Compte non vérifié. Veuillez vérifier votre compte.", verify: true });
         }
-
+    
         const isMatch = await bcrypt.compare(motdepasse, utilisateur.motdepasse);
         if (!isMatch) {
             return res.status(401).send({ message: "Mot de passe incorrect !" });
         }
-
+    
         const payload = {
             user: {
                 id: utilisateur.id_utilisateur,
@@ -200,10 +201,10 @@ exports.login = async (req, res) => {
                 nom: utilisateur.nom,
             }
         };
-
+    
         jwt.sign(
             payload,
-            process.env.JWT_SECRET,
+            process.env.JWT_SECRET, // Assurez-vous que cette ligne utilise bien votre clé secrète
             { expiresIn: 3600 },
             (err, token) => {
                 if (err) throw err;
@@ -215,5 +216,4 @@ exports.login = async (req, res) => {
         );
     } catch (err) {
         res.status(500).send({ message: err.message || "Une erreur est survenue lors de la tentative de connexion." });
-    }
-};
+    };
