0b4c40b449056cad0cdf6e527db002237fcfb4e3
Bouwerqrq Nass <bouwerqrqnass@MacBook-Pro-de-Bouwerqrq.local>
2024-06-18 16:44:14 +0200

    upd

--- controllers/discussions.controller.js
+++ controllers/discussions.controller.js
@@ -16,23 +16,29 @@ exports.findAll = async (req, res) => {
 
 // Créer une nouvelle discussions :
 
-exports.create = async (req, res) => {
-    try {
-        console.log("Requête reçue pour créer une discussion:", req.body); // Log de débogage
 
-        const discussion = await Discussions.create({
+exports.create = async (req, res) => {
+
+        console.log("Requête reçue pour créer une discussion:", req.body); // Log de débogage
+        if (!req.body.titre) {
+            return res.status(400).send({ message: "Tous les champs sont requis: contenu, id_utilisateur, id_bonplan" });
+        }
+        const discussion = {
             titre: req.body.titre,
-            datecreation: req.body.datecreation,
+            datecreation:  new Date() ,
             content: req.body.content,
             likes: req.body.likes,
             id_utilisateur: req.body.id_utilisateur,
-            categoryId: req.body.categoryId
-        });
+            id_category: req.body.id_category
+        };
 
-        console.log("Discussion créée avec succès:", discussion); // Log de succès
-        res.status(201).send(discussion);
-    } catch (e) {
-        console.error("Erreur lors de la création de la discussion:", e); // Log d'erreur détaillé
+        try {
+            const data = await Commentary.create(commentary);
+            res.send(data);
+        } 
+    
+    catch (e) {
+        console.log(e);
         res.status(500).send({ message: "Erreur lors de la création de la discussion." });
     }
 };
